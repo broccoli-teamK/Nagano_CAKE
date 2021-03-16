20d4c826aca77d99a3d10fea7c4ea6516726e329
sumi-re01 <owob.viola@gmail.com>
2021-03-16 13:30:54 +0000

    [Add]customer_status default: true

--- db/migrate/20210316073753_devise_create_customers.rb
+++ db/migrate/20210316073753_devise_create_customers.rb
@@ -39,7 +39,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       t.string :postal_code
       t.string :address
       t.string :phone_number
-      t.boolean :customer_status, null: false
+      t.boolean :customer_status, null: false, default: true
 
       t.timestamps null: false
     end
