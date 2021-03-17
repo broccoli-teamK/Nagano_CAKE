9a64ad6d07d022a96ff19be038fb7dff754e53fb
sumi-re01 <owob.viola@gmail.com>
2021-03-17 00:49:59 +0000

    [Add]管理者・会員側devise view&controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,20 @@ Rails.application.routes.draw do
 # 管理者側のルーティング
 
   # 管理者側ログイン
-  devise_for :admins
+  # devise_for :admins, controllers: {
+  #   sessions: 'admins/sessions',
+  #   passwords:     'admins/passwords'
+  # }
+  devise_for :admins, skip: :all
+  devise_scope :admin do
+  get 'admins/sign_in' => 'admins/sessions#new', as: 'new_admin_session'
+  post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
+  delete 'admins/sign_out' => 'admins/sessions#destroy', as: 'destroy_admin_session'
+  end
 
   # 管理者側機能
   namespace :admin do
+
     get '/' => 'homes#top'
     resources :products, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
@@ -17,7 +27,11 @@ Rails.application.routes.draw do
 
 # 会員側のルーティング
 
-  devise_for :customers
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    passwords: 'customers/passwords',
+    registrations: 'customers/registrations'
+  }
 
   scope module: :public do
     root to: 'homes#top'
