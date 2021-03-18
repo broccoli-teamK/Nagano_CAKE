b0d45d785d794778fea116ed7e8e35c72a94736a
cairo387 <mifuneri@outlook.jp>
2021-03-18 14:16:28 +0000

    [Add]Address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,40 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
   end
 
   def create
+    @address = current_customer.addresses.new(address_params)
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @book = Address.find(params[:id])
+    if @book.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
   end
+  
+  private
+  
+    def address_params
+      params.require(:address).permit(:postal_code, :address, :address_name)
+    end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
 
-  belongs_to :user
+  belongs_to :customer
 
 end
