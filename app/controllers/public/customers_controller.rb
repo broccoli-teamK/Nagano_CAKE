8cc0e2d949ba3d687d767bc7803a8e7bbe9e9651
cairo387 <mifuneri@outlook.jp>
2021-03-18 02:08:29 +0000

    [Add]Customer Pages

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,6 @@
 class Public::CustomersController < ApplicationController
   def show
+   @customer = current_customer
   end
 
   def confirm
@@ -9,8 +10,21 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path(@customer)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:email,:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:phone_number)
   end
 end
