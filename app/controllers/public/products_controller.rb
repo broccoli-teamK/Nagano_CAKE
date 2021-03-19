f300638f7642a3581851208036c0f40c0cf96f89
EC2 Default User <ec2-user@ip-172-31-64-137.ec2.internal>
2021-03-19 05:55:55 +0000

    商品一覧、商品詳細

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,14 @@
 class Public::ProductsController < ApplicationController
   def show
+    @product = Product.find(params[:id])
   end
 
   def index
+    @products = Product.all.page(params[:page]).per(8)
+  end
+  
+  private
+  def products_params
+    params.require(:product).permit(:genre, :name, :introduction, :price, :image, :sales_status)
   end
 end
