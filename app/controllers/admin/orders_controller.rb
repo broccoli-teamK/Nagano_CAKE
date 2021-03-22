847069bccc71cc86175e385115334ff290678d65
sumi-re01 <owob.viola@gmail.com>
2021-03-22 01:23:28 +0000

    [Add]合計金額のメソッド→Orderモデル内に追加. ステータス設定機能実装途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,23 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+
+    @order = Order.find(params[:id])
+
   end
 
   def update
+
+    @order = Order.find(params[:id])
+    @order.save(order_params)
+    redirect_to admin_order_path(@order.id)
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,23 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products, dependent: :destroy
 
+
+
+
+  def total
+    total = 0
+    self.order_products.each do |order_product|
+      total += order_product.sub_total
+    end
+    total
+  end
+
+  enum order_status: {
+    waiting: 0,
+    paid_up: 1,
+    making: 2,
+    preparing: 3,
+    shipped: 4
+  }
+
 end
