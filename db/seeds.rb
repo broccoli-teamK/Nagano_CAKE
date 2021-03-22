f6f7485ca8cd125e2ff57331ff87d2d7131a0155
sumi-re01 <owob.viola@gmail.com>
2021-03-22 01:25:11 +0000

    [seed]サンプル

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,49 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(
-  email: 'Nagano-cake@admin.com',
-  password: 'admin_sign_in'
-  )
+# Admin.create!(
+#   email: 'Nagano-cake@admin.com',
+#   password: 'admin_sign_in'
+#   )
+
+
+# Order.create!(
+#   customer_id: 1,
+#   shipping_postal_code: '1001000',
+#   shipping_address: '住所ここ',
+#   shipping_name: '宛先の名前',
+#   payment_method: 0,
+#   total_price: '1800',
+#   postage: '800',
+#   order_status: 0,
+#   )
+
+# Product.create!(
+#   genre_id: 1,
+#   name: '商品名ここ',
+#   price: 487,
+#   sales_status: true
+#   )
+
+# OrderProduct.create!(
+#   order_id: 1,
+#   product_id: 1,
+#   quantity: 2,
+#   product_status: 0,
+#   tax_in_price: 500,
+#   )
+
+# Product.create!(
+#   genre_id: 1,
+#   name: '商品2',
+#   price: 450,
+#   sales_status: true
+#   )
+
+# OrderProduct.create!(
+#   order_id: 1,
+#   product_id: 2,
+#   quantity: 3,
+#   product_status: 0,
+#   tax_in_price: 1500,
+#   )
