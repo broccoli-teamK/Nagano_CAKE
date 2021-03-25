6fa78c72044ad2a0cb317e59c77c264745c503a0
cairo387 <mifuneri@outlook.jp>
2021-03-25 02:37:01 +0000

    [Fix]各種微調整

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,11 @@
 class Admin::HomesController < ApplicationController
   def top
 
-    @orders = Order.all
-
+    if params[:format].present?
+      @orders = Order.where(customer_id: params[:format]).page(params[:page]).per(10)
+    else
+      @orders = Order.all.page(params[:page]).per(10)
+    end
+    
   end
 end
