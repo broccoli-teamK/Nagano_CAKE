73f457ceb019f9484c37ea15b5cef7fbad028ae1
cairo387 <mifuneri@outlook.jp>
2021-03-26 03:23:36 +0000

    [Fix]各種調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
       case resource
       when Customer
-        customers_my_page_path(resource)
+        root_path(resource)
       when Admin
         admin_path
       end
