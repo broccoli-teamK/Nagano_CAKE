77691b92c93710d72de89d691f17dc66766a959d
sumi-re01 <owob.viola@gmail.com>
2021-03-26 09:42:02 +0000

    [Add]model present:true

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,11 @@
 class Address < ApplicationRecord
+   validates :customer_id, presence: true
+   validates :postal_code, presence: true
+   validates :address, presence: true
+   validates :address_name, presence: true
 
   belongs_to :customer
-  
+
   def full_address
     "〒" + self.postal_code + " " + self.address + " " + self.address_name
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,20 @@
 class Product < ApplicationRecord
+  validates :genre_id, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :sales_status, presence: true
+
   belongs_to :genre
 
   has_many :cart_products, dependent: :destroy
   has_many :order_products, dependent: :destroy
 
   attachment :image
-  
+
   def tax_included_price
     self.price * 1.1
   end
-  
+
 end
 
 
